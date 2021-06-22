4aa3ab8ba599bad779b79a2e0330b34e5c0c0ede
teamA9-pnhs <71691578+teamA9-pnhs@users.noreply.github.com>
2021-06-22 11:18:55 +0530

    Update views.py

--- college_portal_app/views.py
+++ college_portal_app/views.py
@@ -73,7 +73,7 @@ def change_password(request):
         return render(request, 'index3.html', {'message':"Passwords don't match each other",'email':request.POST.get('otp_email')})
 
 
-ef doLogin(request):
+def doLogin (request):
     if request.method != "POST":
         return HttpResponse("<h2>Method Not Allowed</h2>")
         
@@ -87,12 +87,12 @@ ef doLogin(request):
                 return redirect('admin_home')
                 
             elif user_type == '2':
-                #return  HttpResponseRedirect(Reversible("staff_home"))
+                # return HttpResponse("Teacher Login"+str(user.user_type))
                 return redirect('staff_home')
                 
             elif user_type == '3':
-                #return HttpResponse("Student Login")
-               return redirect('student_home')
+                # return HttpResponse("Student Login")
+                return redirect('student_home')
             else:
                 messages.error(request, "Invalid Login!")
                 return redirect('login')
@@ -100,7 +100,6 @@ ef doLogin(request):
             messages.error(request, "Invalid Login Credentials!")
             #return HttpResponseRedirect("/")
             return redirect('login')
- 
 
 
 
