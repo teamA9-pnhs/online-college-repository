8fe8018aec08ebc30352142000072023f1db7b24
HarshatD <70877781+HarshatD@users.noreply.github.com>
2022-01-16 20:10:50 +0530

    Update urls.py

--- college_portal_app/urls.py
+++ college_portal_app/urls.py
@@ -1,112 +1,45 @@
-# from channels.auth import login, logout
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth import get_user_model
-from random import randint
-from django.core.mail import send_mail
-from college_portal_app.EmailBackEnd import EmailBackEnd
+
+from django.urls import path, include
+from . import views
+from . import HodViews
 
 
-def home(request):
-    return render(request, 'index.html')
-
-
-def loginPage(request):
-    return render(request, 'login.html')
-
-def go_here(request):
-    if request.method == 'GET':
-       return render(request , 'index.html')
-
-def reset_password(request):
-    if not 'email' in request.POST is None:
-        email = request.POST.get('email')
-        print(email)
-    else:
-        return redirect('index.html')
-    if not get_user_model().objects.filter(email= email):
-        return render(request, 'otp_not_exist.html' )
-    return render(request, 'index2.html',{'otp1_email': email })
-  
-
-def verify_otp(request):
-    if 'otp_email' in request.POST:
-        print(f"Process of checking the otp {request.POST.get('otp_email')} HANG ON!")
-        user = get_user_model().objects.get(email=request.POST.get('otp_email'))
-        key = user.random_key
-        print(key)
-        if key == request.POST.get('OTP'):
-            return render(request, 'index3.html',{'email':request.POST.get('otp_email')})
-        return render(request, 'otpSignIn.html', {'message':"Your OTP is wrong, Enter Again!",'otp1_email':request.POST.get('otp_email')})
-    return (request, 'otpSignIn.html',{'message':"Well optemail not found",'otp1_email':request.POST.get('otp_email')})
-
-def sending_email(request):
-    if 'otp_email' in request.POST:
-        user= get_user_model().objects.get(email=request.POST.get('otp_email'))
-        user.random_key = randint(100000, 999999)
-        user.save()
-        key = user.random_key
-        email_mesg ='Well you forgot your email So I\'m here now. Your key for new password is ' + str(key)
-        print(email_mesg)
-        # send_mail("Reset Password OTP",email_mesg , email ,[request.POST.get('otp_email')], fal_silently=False)
-        return render(request, "otpSignIn.html",{"otp1_email": request.POST.get('otp_email')})
-    print("not working")
-    return render(request, "index.html")
-
-def change_password(request):
-    if(request.POST.get('password1')==request.POST.get('password2')):
-        User = get_user_model().objects.get(email=request.POST.get('otp_email'))
-        User.set_password(request.POST.get('password1'))
-        User.save()
-        print("well the passwords were same so the password was changed")
-        return render(request, 'login.html',{'message':"Your password has been successfully changed!"})
-    else:
-        print("Things went bad, abort mission!")
-        return render(request, 'index3.html', {'message':"Passwords don't match each other",'email':request.POST.get('otp_email')})
-
-
-def doLogin(request):
-    if request.method != "POST":
-        return HttpResponse("<h2>Method Not Allowed</h2>")
-        
-    else:
-        user = EmailBackEnd.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
-        if user != None:
-            login(request, user)
-            user_type = user.user_type
-            #return HttpResponse("Email: "+request.POST.get('email')+ " Password: "+request.POST.get('password'))
-            if user_type == '1':
-                return redirect('admin_home')
-                
-            elif user_type == '2':
-                # return HttpResponse("Teacher Login"+str(user.user_type))
-                return redirect('staff_home')
-                
-            elif user_type == '3':
-                # return HttpResponse("Student Login")
-                return redirect('student_home')
-            else:
-                messages.error(request, "Invalid Login!")
-                return redirect('login')
-        else:
-            messages.error(request, "Invalid Login Credentials!")
-            #return HttpResponseRedirect("/")
-            return redirect('login')
-
-
-
-def get_user_details(request):
-    if request.user != None:
-        return HttpResponse("User: "+request.user.email+" User Type: "+request.user.user_type)
-    else:
-        return HttpResponse("Please Login First")
-
-
-
-def logout_user(request):
-    logout(request)
-    return HttpResponseRedirect('/')
-
-
+urlpatterns = [
+    path('', views.loginPage, name="login"),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path('admin_home/', HodViews.admin_home, name="admin_home"),
+    path('add_teacher/', HodViews.add_teacher, name="add_teacher"),
+    path('add_teacher_save/', HodViews.add_teacher_save, name="add_teacher_save"),
+    path('add_semister/', HodViews.add_semister, name="add_semister"),
+    path('add_semister_save/', HodViews.add_semister_save, name="add_semister_save"),
+    path('add_student/', HodViews.add_student, name="add_student"),
+    path('add_student_save/', HodViews.add_student_save, name="add_student_save"),
+    path('attendance_student/', HodViews.attendance_student, name="attendance_student"),
+    path('attendance_faculty/', HodViews.attendance_faculty, name="attendance_faculty"),
+    path('change_password/',views.change_password, name='change_password'),
+    path('check_email/', views.go_here, name ="go_here"),
+    path('checked_email/', views.reset_password , name="reset_password"),
+    path('doLogin/', views.doLogin, name="doLogin"),
+    path('delete_semister/<semister_id>/', HodViews.delete_semister, name="delete_semister"),
+    path('edit_semister/<semister_id>/', HodViews.edit_semister, name="edit_semister"),
+    path('edit_semister_save/', HodViews.edit_semister_save, name="edit_semister_save"),
+    path('edit_teacher/<teacher_id>/', HodViews.edit_teacher, name="edit_teacher"),
+    path('edit_teacher_save/', HodViews.edit_teacher_save, name="edit_teacher_save"),
+    path('delete_teacher/<teacher_id>/', HodViews.delete_teacher, name="delete_teacher"), 
+    path('get_user_details/', views.get_user_details, name="get_user_details"),
+    path('logout_user/', views.logout_user, name="logout_user"),
+    path('leave_req/', HodViews.leave_req, name="leave_req"),
+    path('leave_req_faculty/', HodViews.leave_req_faculty, name="leave_req_faculty"), 
+    path('manage_semister/', HodViews.manage_semister, name="manage_semister"),
+    path('manage_teacher/', HodViews.manage_teacher, name="manage_teacher"),
+    path('manage_student/', HodViews.manage_student, name="manage_student"),
+    path('send_otp/' , views.sending_email , name="sending_email"),
+    path('successful/', views.verify_otp , name="verify_otp"),
+    path('show_calendar/' , HodViews.show_calendar , name='show_calendar'),
+    path('staff_home/', HodViews.staff_home, name="staff_home"),
+    path('student_home/', HodViews.student_home, name="student_home"),
+    # path('edit_student/<student_id>', HodViews.edit_student, name="edit_student"),
+    # path('edit_student_save/', HodViews.edit_student_save, name="edit_student_save"),
+    # path('delete_student/<student_id>/', HodViews.delete_student, name="delete_student"),
+    path(r'new_event/',HodViews.new_event , name='new_event_adder'),
+]
